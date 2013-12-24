f92009112300700f1e0a3466cbb82d3a21a8dcbf
Soares Chen <soares.chen@gmail.com>
2013-12-25 03:42:52 +0800

    Attempt to fix callback timeout.

--- test/timeout.js
+++ test/timeout.js
@@ -3,22 +3,25 @@
 
 var should = require('should')
 var safeCallbackLib = require('../lib/safe-callback')
+
 var safeCallback = safeCallbackLib.safeCallback
+var safeFunction = safeCallbackLib.safeAsyncFunction
 safeCallbackLib.setCallbackTimeout(2000)
 
 var func1 = function(callback) {
   // pretend to forget callback
 }
 
-var func2 = function(callback) {
-  func1(safeCallback(callback))
+var func2 = function(func, callback) {
+  func = safeFunction(func)
+  func(callback)
 }
 
 var func3 = function(callback) {
   callback = safeCallback(callback)
 
   setImmediate(function() {
-    func2(callback)
+    func2(func1, callback)
   })
 }
 
